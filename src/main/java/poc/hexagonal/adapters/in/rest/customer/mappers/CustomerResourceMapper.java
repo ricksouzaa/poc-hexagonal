63fe4e1c5ab08bf865ffc772844ed8c6a5e43f63
Ricardo Antonio de Souza <rick.souzaa@gmail.com>
2023-05-13 01:51:19 -0400

    Improvements

--- src/main/java/poc/hexagonal/adapters/in/rest/customer/mappers/CustomerResourceMapper.java
+++ src/main/java/poc/hexagonal/adapters/in/rest/customer/mappers/CustomerResourceMapper.java
@@ -1,18 +1,11 @@
 package poc.hexagonal.adapters.in.rest.customer.mappers;
 
-import static org.mapstruct.InjectionStrategy.CONSTRUCTOR;
-import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
-
 import org.mapstruct.Mapper;
-
 import poc.hexagonal.adapters.in.rest.customer.dtos.request.CustomerRequest;
 import poc.hexagonal.adapters.in.rest.customer.dtos.response.CustomerResponse;
 import poc.hexagonal.application.core.domain.customer.models.Customer;
 
-@Mapper(
-    componentModel = SPRING,
-    injectionStrategy = CONSTRUCTOR
-)
+@Mapper
 public interface CustomerResourceMapper {
   CustomerResponse toDto(Customer customer);
 
